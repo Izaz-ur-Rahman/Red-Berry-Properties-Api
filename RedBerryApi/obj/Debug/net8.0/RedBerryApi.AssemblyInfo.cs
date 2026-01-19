09813594d67bee9ad108ef4d641bb184fe71bdf6
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2026-01-19 10:35:40 +0500

    install 3 packege for database

--- RedBerryApi/obj/Debug/net8.0/RedBerryApi.AssemblyInfo.cs
+++ RedBerryApi/obj/Debug/net8.0/RedBerryApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RedBerryApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ed9e0d175e4b32e957a373513edca135d7932fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RedBerryApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RedBerryApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
